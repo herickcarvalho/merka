c0cfe4b51aae783d0e3481e6614dc6e60a754b51
PC <herickmatheussoaresdecarvalho@gmail.com>
2026-07-27 21:17:14 -0300

    feat(product): implementa estrutura inicial da entidade e validações de nome

--- backend/src/Modules/Products/Mercado.Modules.Products.Domain/Entities/product.cs
+++ backend/src/Modules/Products/Mercado.Modules.Products.Domain/Entities/product.cs
@@ -1,4 +1,4 @@
-
+using Mercado.Modules.Products.Domain.Exceptions;
 
 namespace Mercado.Modules.Products.Domain.Entities;
 
@@ -6,77 +6,109 @@ public class Product
 {
     public Guid Id { get; private set; }
 
-    public string Name { get; private set;} 
+    public string Name { get; private set; }
 
     public string Description { get; private set; }
 
-    public string SKU {get; private set;  }
+    public string SKU { get; private set; }
 
-    public string? Barcode { get; private set;}
+    public string? Barcode { get; private set; }
 
-    public Guid CategoryId { get; private set;}
+    public Guid CategoryId { get; private set; }
 
-    public Guid BrandId { get; private set;}
+    public Guid? BrandId { get; private set; }
 
-    public decimal CostPrice {get; private set;}
+    public decimal CostPrice { get; private set; }
 
-    public decimal SalePrice {get; private set;}
+    public decimal SalePrice { get; private set; }
 
-    public int MinimumStock {get; private set;}
+    public int MinimumStock { get; private set; }
 
-    public bool IsActive {get; private set;}
+    public bool IsActive { get; private set; }
 
-    public DateTime CreatedAt {get; private set;}
+    public DateTime CreatedAt { get; private set; }
 
-    public DateTime? UpdatedAt {get; private set;}
+    public DateTime? UpdatedAt { get; private set; }
 
-
-
-    public Product (
+    public Product(
         string name,
         string description,
         string sku,
         Guid categoryId,
-        Guid brandId,
+        Guid? brandId,
         decimal costPrice,
         decimal salePrice,
         string? barcode,
-        int minimumStock
-        )
-
-        private void ValidateName(Name)
-{
-    if (string.IsNullOrWhiteSpace(name))
+        int minimumStock)
     {
-        throw new ProductDomainException("O nome do produto é obrigatório.");
-    } 
-}
+        ValidateName(name);
 
-ValidateName(name);
-        
+        Id = Guid.NewGuid();
+
+        Name = name;
+        Description = description;
+        SKU = sku;
+        CategoryId = categoryId;
+        BrandId = brandId;
+        CostPrice = costPrice;
+        SalePrice = salePrice;
+        Barcode = barcode;
+        MinimumStock = minimumStock;
+
+        IsActive = true;
+        CreatedAt = DateTime.UtcNow;
+
+
+          public void ChangeMinimumStock(int newMinimumStock)
         {
-
-            Id = Guid.NewGuid();
-
-
-            Name = name;
-         
-            Description = description;
-            SKU = sku;
-            CategoryId = categoryId;
-            BrandId = brandId;
-            CostPrice = costPrice;
-            SalePrice = salePrice;
-            Barcode = barcode;
-            MinimumStock = minimumStock;
-            
-            IsActive = true;  
-            CreatedAt = DateTime.UtcNow;
+    if (newMinimumStock < 0)
+        }
+    
+        throw new ProductDomainException("...");
     }
 
+       MinimumStock=newMinimumStock;
+      
+            }    
 
+            public void changeName(string newName)
+            {
+                if(string.IsNullOrWhiteSpace(newName) )
+                {
+                throw new ProductDomainException("O nome do produto é obrigatório");
+                }
+
+                if(newName.Length < 3 )
+                 {
+
+               throw new ProductDomainException("O produto deve possuir pelo menos 3 caracteres");
+               }
+
+               if (Name == newName)
+                  {
+                throw new ProductDomainException("O novo nome deve ser diferente do nome atual.");
+                   }
+            }
+
+          
+            Name = newName;
+               
+               }
+
+    private void ValidateName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ProductDomainException(
+                "O nome do produto é obrigatório.");
+        }
+    }
 }
 
+public void Deactivate()
+{
+    isActive = false;
+}
 
 
 
